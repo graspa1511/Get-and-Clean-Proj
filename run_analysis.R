9f2740a7e0b6f82589086b4f2b3e523f3213d5b0
Guillermo <graspa1511@gmail.com>
2015-12-27 19:21:01 -0500

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,18 +2,18 @@
 
 run_analysis <- function(){
   # loading test 
-  subject_test = read.table("UCI HAR Dataset/test/subject_test.txt")
-  X_test = read.table("UCI HAR Dataset/test/X_test.txt")
-  Y_test = read.table("UCI HAR Dataset/test/Y_test.txt")
+  subject_test = read.table("C:/Users/Guillermo/Documents/UCI HAR Dataset/test/subject_test.txt")
+  X_test = read.table("C:/Users/Guillermo/Documents/UCI HAR Dataset/test/X_test.txt")
+  Y_test = read.table("C:/Users/Guillermo/Documents/UCI HAR Dataset/test/Y_test.txt")
   
   # loading train
-  subject_train = read.table("UCI HAR Dataset/train/subject_train.txt")
-  X_train = read.table("UCI HAR Dataset/train/X_train.txt")
-  Y_train = read.table("UCI HAR Dataset/train/Y_train.txt")
+  subject_train = read.table("C:/Users/Guillermo/Documents/UCI HAR Dataset/train/subject_train.txt")
+  X_train = read.table("C:/Users/Guillermo/Documents/UCI HAR Dataset/train/X_train.txt")
+  Y_train = read.table("C:/Users/Guillermo/Documents/UCI HAR Dataset/train/Y_train.txt")
   
   # load features info
-  features <- read.table("UCI HAR Dataset/features.txt", col.names=c("featureId", "featureLabel"))
-  activities <- read.table("UCI HAR Dataset/activity_labels.txt", col.names=c("activityId", "activityLabel"))
+  features <- read.table("C:/Users/Guillermo/Documents/UCI HAR Dataset/features.txt", col.names=c("featureId", "featureLabel"))
+  activities <- read.table("C:/Users/Guillermo/Documents/UCI HAR Dataset/activity_labels.txt", col.names=c("activityId", "activityLabel"))
   activities$activityLabel <- gsub("_", "", as.character(activities$activityLabel))
   includedFeatures <- grep("-mean\\(\\)|-std\\(\\)", features$featureLabel)
   
